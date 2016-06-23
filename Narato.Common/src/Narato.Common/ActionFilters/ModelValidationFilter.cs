d0f9297e85f783b8ba5d3d6ec7178c3668205fae
Bart ten Velde <bart.tenvelde@narato.be>
2016-06-23 14:52:56 +0200

    added ModelValidationFilter

---
+++ Narato.Common/src/Narato.Common/ActionFilters/ModelValidationFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Narato.Common.Models;
+
+namespace Narato.Common.ActionFilters
+{
+    public class ModelValidationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var response = new Response();
+                AddValidationFeedback(context.ModelState, response);
+                context.Result = new BadRequestObjectResult(response);
+            }
+        }
+
+        public static void AddValidationFeedback(ModelStateDictionary modelState, Response response)
+        {
+            foreach (var modelstateItem in modelState)
+            {
+                foreach (var error in modelstateItem.Value.Errors)
+                {
+                    response.Feedback.Add(new FeedbackItem
+                    {
+                        Type = FeedbackType.Error,
+                        Description = $"{modelstateItem.Key}: {error.ErrorMessage}"
+                    });
+                }
+            }
+        }
+    }
+}
