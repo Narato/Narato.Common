d171ec05aad62ab2d51b8920db1cb36a11557290
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2016-06-30 11:32:52 +0200

    added opencover script

---
+++ Narato.Common/RunOpenCoverAndReportGenerator.ps1
@@ -0,0 +1,7 @@
+cd $env:USERPROFILE\Source\Repos\narato-common\Narato.Common\tests\Narato.Common.Test
+&"$env:USERPROFILE\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" "-target:${env:programfiles}\dotnet\dotnet.exe" "-targetargs: test" "-register:User" "-filter:+[Narato.Common*]* -[*.Test]*" "-output:Unit_Test_Coverage_Report.xml"
+
+
+&"$env:USERPROFILE\.nuget\packages\ReportGenerator\2.4.5\tools\ReportGenerator.exe" -reports:"$env:USERPROFILE\Source\Repos\narato-common\Narato.Common\tests\Narato.Common.Test\Unit_Test_Coverage_Report.xml" -targetdir:"$env:USERPROFILE\Source\Repos\narato-common\Narato.Common\UnitTestCoverage_Report"
+
+cd $env:USERPROFILE\Source\Repos\narato-common\Narato.Common\
