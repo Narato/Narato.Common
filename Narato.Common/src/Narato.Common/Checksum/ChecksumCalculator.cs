76ee7a3fd3392331a879be93ec9de73cdf877777
Bart Callaerts <Bart Callaerts>
2016-08-16 11:09:02 +0200

    Added checksumCalculator - Bumped to v 1.0.6

---
+++ Narato.Common/src/Narato.Common/Checksum/ChecksumCalculator.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narato.Common.Checksum
+{
+    public class ChecksumCalculator
+    {
+        public string CreateChecksum(object value)
+        {
+            var serializedObjectString = JsonConvert.SerializeObject(value);
+
+            string hash;
+            using (MD5 md5 = MD5.Create())
+            {
+                hash = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(serializedObjectString))).Replace("-", String.Empty);
+            }
+
+            return hash;
+        }
+    }
+}
