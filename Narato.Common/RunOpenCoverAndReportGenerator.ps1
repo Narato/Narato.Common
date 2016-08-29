e8756f00c3adf34b8142245066d97b2fcc614e3b
Steven Baetens <steven.baetens@narato.be>
2016-08-29 16:08:54 +0200

    Temp fix for RunOpenCoverAndReportGenerator.ps1 (-oldStyle)

--- Narato.Common/RunOpenCoverAndReportGenerator.ps1
+++ Narato.Common/RunOpenCoverAndReportGenerator.ps1
@@ -1,5 +1,5 @@
 cd $env:USERPROFILE\Source\Repos\narato-common\Narato.Common\tests\Narato.Common.Test
-&"$env:USERPROFILE\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" "-target:${env:programfiles}\dotnet\dotnet.exe" "-targetargs: test" "-register:User" "-filter:+[Narato.Common*]* -[*.Test]*" "-output:Unit_Test_Coverage_Report.xml"
+&"$env:USERPROFILE\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" "-target:${env:programfiles}\dotnet\dotnet.exe" "-targetargs: test" "-register:User" "-filter:+[Narato.Common*]* -[*.Test]*" "-output:Unit_Test_Coverage_Report.xml" -oldStyle
 
 
 &"$env:USERPROFILE\.nuget\packages\ReportGenerator\2.4.5\tools\ReportGenerator.exe" -reports:"$env:USERPROFILE\Source\Repos\narato-common\Narato.Common\tests\Narato.Common.Test\Unit_Test_Coverage_Report.xml" -targetdir:"$env:USERPROFILE\Source\Repos\narato-common\Narato.Common\UnitTestCoverage_Report"
