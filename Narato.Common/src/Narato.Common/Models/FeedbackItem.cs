c9d2404a23d4761ea7d41583a31a98fe9236c850
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2016-12-20 12:13:00 +0100

    created factory methods for feedbackitem

--- Narato.Common/src/Narato.Common/Models/FeedbackItem.cs
+++ Narato.Common/src/Narato.Common/Models/FeedbackItem.cs
@@ -4,5 +4,41 @@
     {
         public FeedbackType Type { get; set; }
         public string Description { get; set; }
+
+        public static FeedbackItem CreateInfoFeedbackItem(string description)
+        {
+            return new FeedbackItem
+            {
+                Type = FeedbackType.Info,
+                Description = description
+            };
+        }
+
+        public static FeedbackItem CreateWarningFeedbackItem(string description)
+        {
+            return new FeedbackItem
+            {
+                Type = FeedbackType.Warning,
+                Description = description
+            };
+        }
+
+        public static FeedbackItem CreateErrorFeedbackItem(string description)
+        {
+            return new FeedbackItem
+            {
+                Type = FeedbackType.Error,
+                Description = description
+            };
+        }
+
+        public static FeedbackItem CreateValidationErrorFeedbackItem(string description)
+        {
+            return new FeedbackItem
+            {
+                Type = FeedbackType.ValidationError,
+                Description = description
+            };
+        }
     }
 }
