d0996410c591fca06e563c66ebb099c8028ea8ec
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2016-12-27 16:09:50 +0100

    added bundled DI module for in startup.cs

---
+++ Narato.Common/src/Narato.Common/DependencyInjection/NaratoCommonServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Narato.Common.Exceptions;
+using Narato.Common.Factory;
+using Narato.Common.Interfaces;
+using Narato.Common.Mappers;
+
+namespace Narato.Common.DependencyInjection
+{
+    public static class NaratoCommonServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddNaratoCommon(this IServiceCollection services)
+        {
+            services.AddTransient<IResponseFactory, ResponseFactory>();
+            services.AddTransient<IExceptionHandler, ExceptionHandler>();
+            services.AddTransient<IExceptionToActionResultMapper, ExceptionToActionResultMapper>();
+            return services;
+        }
+    }
+}
