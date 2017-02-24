ed09cc412913d0408be455ce9c13591a771f62c4
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2017-02-24 14:50:41 +0100

    added logExtensions for early stacktraces in async situations

---
+++ Narato.Common/src/Narato.Common/Logging/LogExtensions.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using System;
+using System.Threading.Tasks;
+
+namespace Narato.Common.Logging
+{
+    public static class LogExtensions
+    {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public async static Task<T> LogOnException<T>(this Task<T> task)
+        {
+            try
+            {
+                return await task;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("early stack: " + ex.StackTrace);
+                throw ex;
+            }
+        }
+
+        public async static Task LogOnException(this Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("early stack: " + ex.StackTrace);
+                throw ex;
+            }
+        }
+    }
+}
